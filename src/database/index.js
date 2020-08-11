a1f4b0b7aafb078a833edc39c1ac4de5bca8687a
Wellvergton Marques <wmc.jp@outlook.com>
2020-08-11 19:04:05 -0300

    Created and initialized Session model

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
+const Session = require("../models/Session");
 const Todo = require("../models/Todo");
 const User = require("../models/User");
 
 const connection = new Sequelize(dbConfig);
 
+Session.init(connection);
 Todo.init(connection);
 User.init(connection);
 
