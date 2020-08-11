464da58d48e573922a93122f3f9e07374f90526d
Wellvergton Marques <wmc.jp@outlook.com>
2020-08-11 16:55:59 -0300

    Created basic session management

--- src/app.js
+++ src/app.js
@@ -2,12 +2,34 @@ require("dotenv").config();
 require("./database");
 
 const express = require("express");
-const app = express();
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./routes");
 
-const port = process.env.PORT;
+const app = express();
+const store = new SequelizeStore({
+  db: require("./database"),
+  tableName: "sessions",
+  checkExpirationInterval: 1000 * 60 * 5,
+});
 
+app.use(
+  session({
+    secret: [
+      process.env.CURRENT_SESSION_SECRET,
+      process.env.PAST_SESSION_SECRET,
+    ],
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 5,
+    },
+  })
+);
 app.use(express.json());
 app.use("/", routes);
 
-app.listen(port);
+store.sync();
+
+app.listen(process.env.PORT);
